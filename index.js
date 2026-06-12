158fcac049eaa7a0f2ce674f87422f9d93811b9f
parvez-nur-shadhin <parveznur00@gmail.com>
2026-06-12 18:13:50 +0600

    api jobs get and post method created

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("The Hireloop Server is running");
+})
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const database = client.db("hire_loop");
+    const jobsCollection = database.collection("jobs");
+    const companyCollection = database.collection("companies");
+
+
+    app.post('/api/jobs', async(req, res) => {
+        const job = req.body;
+        const result = await jobsCollection.insertOne(job);
+        res.send(result);
+    })
+
+    app.get('/api/jobs', async(req, res) => {
+        const query = {};
+
+        if(req.query.companyId){
+            query.companyId = req.query.companyId;
+        }
+        if(req.query.status){
+            query.status = req.query.status;
+        }
+
+        const cursor = jobsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result);
+
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`);
+})
